ab9c3c9e5fc50a2e3892b53c1d6b4d4fbd563749
Mike <m.p.mckew@gmail.com>
2022-03-23 15:42:27 -0400

    Implement basic functionality

---
+++ pwdgen.py
@@ -0,0 +1,14 @@
+#! python3
+
+import string, random, pyperclip
+
+def main():
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''
+    for _ in range(0, 15):
+        password += random.choice(chars)
+    print(password)
+    pyperclip.copy(password)
+
+if __name__ == "__main__":
+    main()
