99bd1e34577a05c01dea0ae346ccfb135400c373
Mike <m.p.mckew@gmail.com>
2022-03-30 08:21:34 -0400

    Check that pwds inc upper, lower, digit, and punc

--- pwdgen.py
+++ pwdgen.py
@@ -5,9 +5,16 @@ import string, secrets, pyperclip
 def main():
     chars = string.ascii_letters + string.digits + string.punctuation
     password = ''
-    for _ in range(0, 15):
+    for _ in range(15):
         password += secrets.choice(chars)
-    pyperclip.copy(password)
+    if (True in [char.islower() for char in password]
+        and True in [char.isupper() for char in password]
+        and True in [char.isdigit() for char in password]
+        and True in [(char in string.punctuation) for char in password]
+    ):
+        pyperclip.copy(password)
+    else:
+        main()
 
 if __name__ == "__main__":
     main()
