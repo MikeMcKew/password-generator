d943c4447692b4fb5ce68de7a1f7bddcc5810e03
Mike <m.p.mckew@gmail.com>
2022-03-30 08:42:03 -0400

    Clean up password iteration and list comps

--- pwdgen.py
+++ pwdgen.py
@@ -4,14 +4,11 @@ import string, secrets, pyperclip
 
 def generate_password():
     chars = string.ascii_letters + string.digits + string.punctuation
-    password = ''
-    for _ in range(15):
-        password += secrets.choice(chars)
-    if (True in [char.islower() for char in password]
-        and True in [char.isupper() for char in password]
-        and True in [char.isdigit() for char in password]
-        and True in [(char in string.punctuation) for char in password]
-    ):
+    password = ''.join(secrets.choice(chars) for _ in range(15))
+    if (any(char.islower() for char in password) and
+            any(char.isupper() for char in password) and
+            any(char.isdigit() for char in password) and
+            any(char in string.punctuation) for char in password):
         pyperclip.copy(password)
     else:
         generate_password()
