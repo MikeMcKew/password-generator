2164e586b5747df693aadcba1d320005ba1e970f
Mike <m.p.mckew@gmail.com>
2022-03-30 09:10:28 -0400

    Refactor to allow pwd_len to change

--- pwdgen.py
+++ pwdgen.py
@@ -3,12 +3,13 @@
 import string, secrets, pyperclip
 
 def generate_password():
+    pwd_len = 15
     chars = string.ascii_letters + string.digits
-    password = ''.join(secrets.choice(chars) for _ in range(15))
+    password = ''.join(secrets.choice(chars) for _ in range(pwd_len))
     if (any(char.islower() for char in password) and
             any(char.isupper() for char in password) and
             any(char.isdigit() for char in password)):
-        password = password.replace(password[secrets.choice(range(15))], secrets.choice(string.punctuation), 1) # replace a random char with special char
+        password = password.replace(password[secrets.choice(range(pwd_len))], secrets.choice(string.punctuation), 1) # replace a random char with special char
         pyperclip.copy(password)
     else:
         generate_password()
